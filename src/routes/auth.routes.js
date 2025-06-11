9942767ccd2c2de7b9666c33de8e8474b27bbc8d
jupiterbania009 <www.jupiterbania009@gmail.com>
2025-06-11 12:31:11 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, getCurrentUser } = require('../controllers/auth.controller');
-const { protect } = require('../middleware/auth.middleware');
+const { authenticateToken } = require('../middleware/auth.middleware');
 
 // Auth routes
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getCurrentUser);
+router.get('/me', authenticateToken, getCurrentUser);
 
 module.exports = router; 
