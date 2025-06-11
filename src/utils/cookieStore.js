955018fe4c66ed45c02696984221992a69b6aee7
jupiterbania009 <www.jupiterbania009@gmail.com>
2025-06-11 18:38:24 +0530

    Update cookieStore.js

--- src/utils/cookieStore.js
+++ src/utils/cookieStore.js
@@ -1,23 +1,37 @@
-const { CookieJar, Cookie } = require('tough-cookie');
-const FileCookieStore = require('tough-cookie-filestore');
-const fs = require('fs');
+const { CookieJar } = require('tough-cookie');
+const fs = require('fs').promises;
 const path = require('path');
 
 // Ensure cookies directory exists
 const cookiesDir = path.join(__dirname, '../cookies');
-if (!fs.existsSync(cookiesDir)) {
-    fs.mkdirSync(cookiesDir, { recursive: true });
-}
 
-const createCookieStore = (username) => {
-    const cookiePath = path.join(cookiesDir, `${username}.json`);
-    
-    // Create cookie file if it doesn't exist
-    if (!fs.existsSync(cookiePath)) {
-        fs.writeFileSync(cookiePath, '{}');
+const createCookieStore = async (username) => {
+    try {
+        // Ensure directory exists
+        await fs.mkdir(cookiesDir, { recursive: true });
+        
+        const cookieJar = new CookieJar();
+        
+        // Return an async-compatible cookie store
+        return {
+            cookieJar,
+            setCookie: async (cookie, url) => {
+                return await cookieJar.setCookie(cookie, url);
+            },
+            getCookies: async (url) => {
+                return await cookieJar.getCookies(url);
+            },
+            removeAllCookies: async () => {
+                const cookies = await cookieJar.getCookies(url);
+                for (const cookie of cookies) {
+                    await cookieJar.removeCookie(url, cookie.key);
+                }
+            }
+        };
+    } catch (error) {
+        console.error('Error creating cookie store:', error);
+        throw error;
     }
-
-    return new CookieJar(new FileCookieStore(cookiePath));
 };
 
 module.exports = { createCookieStore }; 
